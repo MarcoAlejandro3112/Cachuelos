206d77322cc120ef83814c0767d74ad2d0717c78
Gerard Altamirano <35399391+TheJarX@users.noreply.github.com>
2018-10-20 08:47:37 -0500

    Create connection.php

---
+++ php/connection.php
@@ -0,0 +1,11 @@
+<?php
+  try{  
+  	  $host="";
+      $suser="";
+      $spass="";
+      $db="";
+        $link = new PDO('mysql:host='.$host.';dbname='.$db, $suser, $spass);
+  }catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
