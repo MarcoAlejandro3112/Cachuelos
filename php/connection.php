9a0177f4948fca963d5f5db115f95b84eb98114b
Gerard Altamirano <35399391+TheJarX@users.noreply.github.com>
2018-10-20 10:50:56 -0500

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,10 +1,11 @@
 <?php
   try{  
-  	  $host="";
-      $suser="";
-      $spass="";
-      $db="";
+  	  $host="localhost";
+      $suser="id7557222_cachuelos";
+      $spass="123456";
+      $db="id7557222_cachuelos";
         $link = new PDO('mysql:host='.$host.';dbname='.$db, $suser, $spass);
+    echo 'conexión exitosa!'
   }catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
