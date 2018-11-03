7ea3751698648f745b8cd1d472c620d800027a09
Gerard Altamirano <35399391+TheJarX@users.noreply.github.com>
2018-11-02 19:25:18 -0500

    Update postear.php

--- postear.php
+++ postear.php
@@ -2,11 +2,11 @@
 require_once('connection.php');
 $status=array('status'=>'error','context'=>'post','stat'=>false);
 if(isset($_REQUEST['user']) && !empty($_REQUEST('user')) && isset($_REQUEST['pass']) && !empty($_REQUEST['pass']) ){
-	$sql="INSERT INTO post (user,title,content,image,catid,post_date)".
-	"VALUES()";
+	$sql="INSERT INTO post (user,title,content,image,catid,post_date,payment)".
+	"VALUES(:user,:content,:img,:catid,:catid,:date,:pay)";
 	
 		$stm=$link->prepare($sql);
-	   $stm->execute();
+	   $stm->execute(array(":user"=>$_REQUEST['user'],":content"));
 	   
         $result=$stm->fetchAll();
         
