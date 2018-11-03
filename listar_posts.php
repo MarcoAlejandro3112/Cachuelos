51bcf578908f701112018cae6d2a57097bde1180
Gerard Altamirano <35399391+TheJarX@users.noreply.github.com>
2018-11-03 08:17:56 -0500

    Update listar_posts.php

--- listar_posts.php
+++ listar_posts.php
@@ -3,7 +3,7 @@ require_once('connection.php');
 
 
 
-	$sql="SELECT title,image,u.email email ,content,c.name category,post_date FROM post p, user u ,category c ";
+	$sql="SELECT title,image,u.email email ,content,c.name category,post_date FROM post p INNER JOIN user u ON p.user=u.uid INNER JOIN category c ON p.catid=c.catid";
 	
 		$stm=$link->prepare($sql);
 	   $stm->execute();
@@ -14,7 +14,7 @@ require_once('connection.php');
         
         foreach($result as $row){
           
-            array_push($datos,array("title"=> $row['title'], "user"=> $row['email'], "content"=> $row['content'], "category"=> $row['category'], "imagen"=> $row['image'],"date"=>$row['post_date']));
+            array_push($datos,array("title"=> $row['title'], "user"=> $row['email'], "content"=> $row['content'], "category"=> $row['category'], "imagen"=> $row['image'],"date"=>$row['post_date'],"payment"=>$row['payment']));
         }
         
        echo(json_encode($datos));
