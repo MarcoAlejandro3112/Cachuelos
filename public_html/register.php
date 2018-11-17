4e384a48b63ddbb12cb021bf8a3711dea024ac55
MarcoAlejandro3112 <35712073+MarcoAlejandro3112@users.noreply.github.com>
2018-11-17 11:57:54 -0500

    Add files via upload

---
+++ public_html/register.php
@@ -0,0 +1,26 @@
+<?php
+require_once('connection.php');
+$status= array("status"=>"error","action"=>"register","stat"=>false);
+if(isset($_REQUEST['email']) && !empty($_REQUEST['email']) && isset($_REQUEST['pass']) && 
+!empty($_REQUEST['pass'])&& isset($_REQUEST['pass-confirm']) && !empty($_REQUEST['pass-confirm']) 
+&& $_REQUEST['pass']==$_REQUEST['pass-confirm']){
+    $pass=password_hash($_REQUEST['pass'], PASSWORD_BCRYPT);
+    	$sql="INSERT INTO user(email,pass) VALUES(:email,:pass)";
+        $stm=$link->prepare($sql);
+        
+        try{
+	   $stm->execute(array(":email"=>$_REQUEST['email'],":pass"=>$pass));
+	   
+        if($stm->rowCount()){
+            header('Location: register_details.html');
+        }
+      
+    }catch(PDOException $e){
+        $status=array("status"=>$e->getMessage(),"action"=>"register","stat"=>false);
+            echo json_encode();
+    }
+    
+}else{
+    echo json_encode($status);
+    
+}
