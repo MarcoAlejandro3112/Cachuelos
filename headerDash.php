f642548f44b8001dce2d90a2443f7a3341e7680b
Marco Altamirano <maaltamiranohiga@gmail.com>
2018-11-23 21:26:44 -0500

    avance dashboard

---
+++ headerDash.php
@@ -0,0 +1,27 @@
+<div class="containerNav">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="nav">
+  <a class="navbar-brand" href="index.php">CACHUELOS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="cachuelos.php">Buscar un Cachuelo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="contratar.php">Contratar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="contacto.php">Contacto</a>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0" id="formAcceso">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item float-right">
+        <a class="nav-link " href="#perfil" id="aImg"><img src="img/user.png" alt=""></a>
+      </li>
+      </ul>     
+    </div>
+  </div>
+</nav>
