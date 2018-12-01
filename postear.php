eb57d239eb1f289852bb4295b80187d578da0bb4
Gerard Altamirano <35399391+TheJarX@users.noreply.github.com>
2018-12-01 11:32:52 -0500

    Update postear.php

--- postear.php
+++ postear.php
@@ -2,24 +2,31 @@
 require_once('connection.php');
 
 $status=array('status'=>'error','context'=>'post','stat'=>false);
-if(isset($_SESSION['user']) && !empty($_SESSION('user'))&& isset($_REQUEST['content']) && !empty($_REQUEST['content'])
- && isset($_REQUEST['title']) && !empty($_REQUEST['title']) && isset($_REQUEST['payment']) && !empty($_REQUEST['payment'])  ){
-	$sql="INSERT INTO post (user,title,content,image,post_date,payment)".
-	"VALUES(:user,:content,:img,:date,:pay)";
-	
+if(/*isset($_SESSION['user']) && !empty($_SESSION('user'))&& */isset($_REQUEST['description']) && !empty($_REQUEST['description'])
+  && isset($_REQUEST['title']) && !empty($_REQUEST['title']) && isset($_REQUEST['payment']) && !empty($_REQUEST['payment'])
+   && isset($_REQUEST['type']) && !empty($_REQUEST['type']) && isset($_REQUEST['tags']) && !empty($_REQUEST['tags'])){
+     
+	$sql="INSERT INTO post (user,title,content,post_date,payment,tags,type)".
+	"VALUES(:user,:content:date,:pay,:tags,:type)";
+
 		$stm=$link->prepare($sql);
-       $stm->execute(array(":user"=>$_SESSION['user'],":content"=>$_REQUEST['content'],":img"=>null,
-                            ":date"=>date('Y-m-d'),":pay"=>$_REQUEST['payment']));
-	   
+       $stm->execute(array(":user"=>'elon@musk.com',":content"=>$_REQUEST['description'],
+                            ":date"=>date('Y-m-d'),":pay"=>$_REQUEST['payment']),":tags"=>$_REQUEST['tags']),
+                            ":type"=>$_REQUEST['type']));
+
         $result=$stm->fetchAll();
-        
+
         $datos = array();
-        
+
        $status=array('status'=>'succes','context'=>'post','stat'=>true);
-       
+
         echo(json_encode($status));
 
-        	
+        header('Location:cachuelos.php');
+
+
+          //  print_r($_REQUEST);
+
         }else{
              echo(json_encode($status));
         }
