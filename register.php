3b9c18c1e72e1f53c90f9cbb4074669ad760be18
MarcoAlejandro3112 <35712073+MarcoAlejandro3112@users.noreply.github.com>
2018-12-01 09:53:37 -0500

    pietro corrigelo

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ $status= array("status"=>"error","action"=>"register","stat"=>false);
 if(isset($_REQUEST['email']) && !empty($_REQUEST['email']) && isset($_REQUEST['pass']) && 
 !empty($_REQUEST['pass'])&& isset($_REQUEST['pass-confirm']) && !empty($_REQUEST['pass-confirm']) 
 && $_REQUEST['pass']==$_REQUEST['pass-confirm']){
-    $pass=password_hash($_REQUEST['pass'], PASSWORD_BCRYPT);
+    $pass=$_REQUEST['pass'];
     	$sql="INSERT INTO user(email,pass) VALUES(:email,:pass)";
         $stm=$link->prepare($sql);
         
--- register_details.php
+++ register_details.php
@@ -6,32 +6,32 @@ session_start();
 $status= array("status"=>"error","action"=>"register","stat"=>false);
 if(isset($_FILES)  &&  isset($_REQUEST['name']) && !empty($_REQUEST['name']) && isset($_REQUEST['surname']) && !empty($_REQUEST['surname']) && isset($_REQUEST['id']) && !empty($_REQUEST['id']) && isset($_REQUEST['tel']) && !empty($_REQUEST['tel']) && isset($_REQUEST['country']) && !empty($_REQUEST['country']) && isset($_REQUEST['country']) && !empty($_REQUEST['country']) && isset($_REQUEST['profile']) && !empty($_REQUEST['profile'])){
     			
- $sql='INSERT INTO user_details(user,name,surname,phone,descr,profile,document,country,img) VALUES(:us,:name,:surname,:pho,:des,:ty,:do,:co,:im)';
+ $sql='INSERT INTO user_details(user,name,surname,phone,descr,profile,document,country,img,date) VALUES(:us,:name,:surname,:pho,:des,:ty,:do,:co,:im,now())';
 
         $stm= $link->prepare($sql);
            
         
-        try{$stm->execute(array(':us' =>$email,":name"=>$_REQUEST['name'],":surname"=>$_REQUEST['surname'],":pho"=>$_REQUEST['tel'] , ":des"=>$_REQUEST['description'],":ty"=>$_REQUEST['profile'],":do"=>$_REQUEST['id'],":co"=>"PE",":im"=>date('Ymhs').basename( $_FILES['uploadedfile']['name'])));
+        try{$stm->execute(array(':us' =>$email,":name"=>$_REQUEST['name'],":surname"=>$_REQUEST['surname'],":pho"=>$_REQUEST['tel'] , ":des"=>$_REQUEST['description'],":ty"=>$_REQUEST['profile'],":do"=>$_REQUEST['id'],":co"=>"PE",":im"=>addslashes(file_get_contents($_FILES['uploadedfile']['tmp_name']))));
 
         if ($stm->rowCount()) {
 
             session_unset();
             session_destroy();
             echo "Exito!";
-            if(!is_uploaded_file($_FILES['uploadedfile']['tmp_name'])){
+            if(!is_uploaded_file(addslashes(file_get_contents($_FILES['uploadedfile']['tmp_name'])))){
 
                 echo "Error al procesar la imagen";
                     exit();
             }
 
              $target_path = "img/";
-        $target_path = $target_path . date('Ymhs').basename( $_FILES['uploadedfile']['name']);
+        $target_path = $target_path . addslashes(file_get_contents($_FILES['uploadedfile']['tmp_name']));
             echo "string";
             
 
            
 
-        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+        if(move_uploaded_file(addslashes(file_get_contents($_FILES['uploadedfile']['tmp_name'])), $target_path)) {
          echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido"; header("Location:index.php#openModal2");
         } else{
         echo "Ha ocurrido un error, trate de nuevo!";
